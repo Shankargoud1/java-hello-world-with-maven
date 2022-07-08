7f28c83d45d0e0768f6884a2ff88e8d4a4913d2b
shankar <shankargajula53@gmail.com>
2022-07-08 14:18:21 +0530

    chenges

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,4 +1,4 @@
- def checkout-git() {
+def checkout_git() {
     
     echo "this is checking out git code."
          git credentialsId: 'first', url: 'git@github.com:Shankardevops1/java-hello-world-with-maven.git'
